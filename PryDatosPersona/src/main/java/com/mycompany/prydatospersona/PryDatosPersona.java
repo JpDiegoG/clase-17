e70048f88d4efc28e5a135b423fc86bea4435a75
JpDiegoG <Urrutia7d@gmail.com>
2025-01-17 11:11:25 -0500

    Add files via upload

---
+++ PryDatosPersona/src/main/java/com/mycompany/prydatospersona/PryDatosPersona.java
@@ -0,0 +1,11 @@
+package com.mycompany.prydatospersona;
+
+import Vista.DatosPersonaVista;
+
+public class PryDatosPersona {
+
+    public static void main(String[] args) {
+        DatosPersonaVista dpv=new DatosPersonaVista();
+        dpv.setVisible(true);
+    }
+}
